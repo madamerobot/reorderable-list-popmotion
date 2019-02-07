67340eecd20efbc6826ae6ed68e4e3e86e4d6244
Valerie Fuchs <valerie@yourmajesty.co>
2019-02-07 13:42:55 +0100

    First barebone set-up of list items

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import posed from 'react-pose'
+
+const Box = posed.div({
+  draggable: 'y',
+  init: { scale: 1 },
+  drag: { scale: 1.2 },
+  dragEnd: {
+    y: 0,
+    transition: { type: 'spring', stiffness: 1000, damping: 15 }
+  }
+})
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    // this.state = {
+    //   isVisible: 'hidden'
+    // }
+  }
+
+  handleMouseDown(e) {
+    console.log('Down')
+    // this.setState({
+    //   isVisible: 'visible'
+    // })
+  }
+
+  handleMouseUp(e) {
+    console.log('Up')
+    // this.setState({
+    //   isVisible: 'hidden'
+    // })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App" >
+        <div className="center-container">
+          < Box className="box"/>
+          < Box className="box"/>
+          < Box className="box"/>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
