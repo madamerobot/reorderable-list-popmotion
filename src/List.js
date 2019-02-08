d76f4ed7add3a6c748502a78de1d8a66e4e767ec
Valerie Fuchs <valerie@yourmajesty.co>
2019-02-08 14:44:53 +0100

    It actually works

--- src/List.js
+++ src/List.js
@@ -3,9 +3,9 @@ import './App.css'
 import posed, { PoseGroup } from 'react-pose'
 
 const Item = posed.li({
-  // draggable: 'y',
-  // init: { scale: 1 },
-  // drag: { scale: 1.2 },
+  draggable: 'y',
+  init: { scale: 1 },
+  drag: { scale: 1.2 },
   // dragEnd: {
   //   y: 0,
   //   transition: { type: 'spring', stiffness: 1000, damping: 15 }
@@ -23,12 +23,33 @@ function shuffle(array, currentIndex, newIndex) {
   return array
 }
 
+function mapRows() {
+  const allItems = [].slice.call(document.querySelectorAll('.box'))
+  let rowMap = []
+  allItems.forEach((item, index) => {
+    let rowTopY = item.getBoundingClientRect().y
+    let itemHeight = item.getBoundingClientRect().height
+    let rowBottomY = rowTopY + itemHeight
+    rowMap[index] = [rowTopY, rowBottomY]
+  })
+  return rowMap
+}
+
+function checkRange(value, min, max) {
+  return ((value - min) * (value - max) <= 0)
+}
+
 class List extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      items: [ 
-        'Wash panties' , 
+      mouseY: 0,
+      isPressed: false,
+      draggedItem: null,
+      newRow: null,
+      rowMap: [],
+      items: [
+        'Wash panties',
         'Call Mama',
         'Tidy up drawers',
         'Call Oma'
@@ -36,36 +57,63 @@ class List extends Component {
     }
   }
 
-  handleMouseDown(e) {
-    const currentIndex = e.target.id
-    const newIndex = 3
-    this.reshuffleArray(currentIndex, newIndex)
+  componentDidMount() {
+    const rowMap = mapRows()
+    this.setState({
+      rowMap: rowMap
+    })
   }
 
-  handleMouseUp(e) {
-    console.log('Up')
+  onDragStart(e) {
+    this.setState({
+      isPressed: true,
+      draggedItem: e.target.id
+    })
   }
+
+  trackRow(e) {
+    let newRow = 0
+    this.state.rowMap.forEach((row, index) => {
+      if (checkRange(e.clientY, row[0], row[1])) {
+        newRow = index
+      }
+    })
+    this.setState({
+      newRow: newRow
+    })
+  }
+
+  onDragEnd(e) {
+    this.trackRow(e)
+    if (this.state.newRow !== this.state.draggedItem) {
+      this.reshuffleArray(this.state.draggedItem, this.state.newRow)
+    }
+  }
+
 
   reshuffleArray = ((currentIndex, newIndex) => {
     this.setState(
-      { 
-        items: shuffle(this.state.items, currentIndex, newIndex) 
+      {
+        items: shuffle(this.state.items, currentIndex, newIndex)
       }
     )
   })
 
   render() {
     return (
-      <ul 
-        className="sidepanel"
-        onMouseDown={(e) => this.handleMouseDown(e)}
-        onMouseUp={(e) => this.handleMouseUp(e)}
+      <ul className="sidepanel"
       >
         <PoseGroup>
           {this.state.items.map((item, index) => (
-            <Item 
-              // onValueChange={{ y: y => console.log(y) }}
-              className="box" id={index} data-key={item} key={item}>{item}
+            <Item
+              className="box" 
+              id={index} 
+              data-key={item} 
+              key={item}
+              onDragEnd={(e) => this.onDragEnd(e)}
+              onDragStart={(e) => this.onDragStart(e)}
+              >
+                {item}
             </Item>
           ))}
         </PoseGroup>
