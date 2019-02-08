81dc285d4ed50a9a7b494dcc5e8db342d1d91857
Valerie Fuchs <valerie@yourmajesty.co>
2019-02-08 15:19:48 +0100

    Simplified code

--- src/List.js
+++ src/List.js
@@ -2,48 +2,20 @@ import React, { Component } from 'react'
 import './App.css'
 import posed, { PoseGroup } from 'react-pose'
 
+//Defining Pose Item with Spring Animation config
 const Item = posed.li({
   draggable: 'y',
   init: { scale: 1 },
   drag: { scale: 1.2 },
-  // dragEnd: {
-  //   y: 0,
-  //   transition: { type: 'spring', stiffness: 1000, damping: 15 }
-  // },
   flip: {
     transition: { type: 'spring', stiffness: 30, damping: 6 }
   }
-}
-)
-
-function shuffle(array, currentIndex, newIndex) {
-  let temporaryValue = array[currentIndex]
-  array[currentIndex] = array[newIndex]
-  array[newIndex] = temporaryValue
-  return array
-}
-
-function mapRows() {
-  const allItems = [].slice.call(document.querySelectorAll('.box'))
-  let rowMap = []
-  allItems.forEach((item, index) => {
-    let rowTopY = item.getBoundingClientRect().y
-    let itemHeight = item.getBoundingClientRect().height
-    let rowBottomY = rowTopY + itemHeight
-    rowMap[index] = [rowTopY, rowBottomY]
-  })
-  return rowMap
-}
-
-function checkRange(value, min, max) {
-  return ((value - min) * (value - max) <= 0)
-}
+})
 
 class List extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      mouseY: 0,
       isPressed: false,
       draggedItem: null,
       newRow: null,
@@ -58,7 +30,14 @@ class List extends Component {
   }
 
   componentDidMount() {
-    const rowMap = mapRows()
+    const allItems = [].slice.call(document.querySelectorAll('.box'))
+    let rowMap = []
+    allItems.forEach((item, index) => {
+      const rowTopY = item.getBoundingClientRect().y
+      const itemHeight = item.getBoundingClientRect().height
+      const rowBottomY = rowTopY + itemHeight
+      rowMap[index] = [rowTopY, rowBottomY]
+    })
     this.setState({
       rowMap: rowMap
     })
@@ -66,7 +45,6 @@ class List extends Component {
 
   onDragStart(e) {
     this.setState({
-      isPressed: true,
       draggedItem: e.target.id
     })
   }
@@ -74,35 +52,43 @@ class List extends Component {
   trackRow(e) {
     let newRow = 0
     this.state.rowMap.forEach((row, index) => {
-      if (checkRange(e.clientY, row[0], row[1])) {
+      let value = e.clientY
+      let min = row[0]
+      let max = row[1]
+      let isInRange = (value - min) * (value - max) <= 0
+      if (isInRange) {
         newRow = index
       }
     })
-    this.setState({
-      newRow: newRow
-    })
+    return newRow
   }
 
   onDragEnd(e) {
-    this.trackRow(e)
-    if (this.state.newRow !== this.state.draggedItem) {
-      this.reshuffleArray(this.state.draggedItem, this.state.newRow)
+    let newRow = this.trackRow(e)
+    if (newRow !== this.state.draggedItem) {
+      this.reshuffleArray(this.state.draggedItem, newRow)
     }
   }
 
-
-  reshuffleArray = ((currentIndex, newIndex) => {
+  reshuffleArray (currentIndex, newIndex) {
+    const array = this.state.items
+    let newArray = []
+    if (newIndex !== currentIndex) {
+      let temporaryValue = array[currentIndex]
+      array[currentIndex] = array[newIndex]
+      array[newIndex] = temporaryValue
+      newArray = array
+    }
     this.setState(
       {
-        items: shuffle(this.state.items, currentIndex, newIndex)
+        items: newArray
       }
     )
-  })
+  }
 
   render() {
     return (
-      <ul className="sidepanel"
-      >
+      <ul className="list-wrapper">
         <PoseGroup>
           {this.state.items.map((item, index) => (
             <Item
@@ -113,7 +99,7 @@ class List extends Component {
               onDragEnd={(e) => this.onDragEnd(e)}
               onDragStart={(e) => this.onDragStart(e)}
               >
-                {item}
+              <span className="prio-label">{index + 1}</span>{item}
             </Item>
           ))}
         </PoseGroup>
